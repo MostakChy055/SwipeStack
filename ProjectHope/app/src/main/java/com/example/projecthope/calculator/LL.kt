2c35d187a8edda13066c0016879a5becc46a3ba4
MostakChy055 <mostakchy055@gmail.com>
2025-01-18 02:31:12 +0600

    first commit

---
+++ ProjectHope/app/src/main/java/com/example/projecthope/calculator/LL.kt
@@ -0,0 +1,21 @@
+package com.example.projecthope.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projecthope.R
+
+class LL : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ll)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
